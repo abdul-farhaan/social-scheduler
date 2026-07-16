986fae2d4cec71b80f7cf6dc8a10fe675a719e20
abdul-farhaan <abdulfarhaan0327@gmail.com>
2026-07-16 05:36:36 +0530

    fix: Correct import case for User model in authController

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { User } from "../models/user.js";
+import { User } from "../models/User.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
